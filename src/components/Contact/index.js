75270b78bdb3eab2e47909a0159ae62de3b26f1f
Nika Gedenidze <97714956+Ngedenidze@users.noreply.github.com>
2023-07-13 14:13:39 -0400

    new contact page ```

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -52,36 +52,25 @@ const Contact = () => {
     <>
       <div className="container contact-page">
         <div className="text-zone">
-          {/* <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1> */}
-          <p>
-            I am interested in freelance opportunities - especially on ambitious
-            or large projects. However, if you have any other requests or
-            questions, don't hesitate to contact me using below form either.
-          </p>
           <div className="contact-form">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1>
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['F', 'o', 'r', 'm']}
+                idx={15}
+              />
+            </h1>
             <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
-                  <input placeholder="Name" type="text" name="name" required />
+                  <input placeholder="Name" type="text" name="name" autocomplete="off" required />
                 </li>
                 <li className="half">
                   <input
                     placeholder="Email"
                     type="email"
                     name="email"
+                    autocomplete="off"
                     required
                   />
                 </li>
@@ -90,6 +79,7 @@ const Contact = () => {
                     placeholder="Subject"
                     type="text"
                     name="subject"
+                    autocomplete="off"
                     required
                   />
                 </li>
@@ -97,19 +87,43 @@ const Contact = () => {
                   <textarea
                     placeholder="Message"
                     name="message"
+                    autocomplete="off"
                     required
                   ></textarea>
                 </li>
-                
-                  <input type="submit" className="flat-button" value="SEND" />
+
+                <input type="submit" className="flat-button" value="SEND" />
               </ul>
             </form>
           </div>
         </div>
 
-       <div className="info-map">
+        <div className="info-map">
+          <div class="card">
+            <div class="card-photo"></div>
+            <div class="card-title">Nika Gedenidze <br />
+              <span>Software Engineer</span>
+            </div>
+            <div class="card-socials">
+              <span>
+              Caldwell, NJ
+              </span>
+              <br/>
+              <span>
+              07006
+              </span>
+              <br/>
+              <span>
+              +1 (717) 775 - 0711
+              </span>
+              <br/>
+              <span>
+               Ngedenidze@outlook.com
+              </span>
+            </div>
+          </div>
           {/* <spline-viewer loading-anim url="https://prod.spline.design/IYIxP9mUUv8oXxlY/scene.splinecode"></spline-viewer> */}
-        </div> 
+        </div>
 
 
       </div>
