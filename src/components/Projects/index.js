98d4c03201d4ff8449046d684b037ac7e12b237f
Nika Gedenidze <97714956+Ngedenidze@users.noreply.github.com>
2023-07-31 22:50:51 -0400

    new project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,15 +1,42 @@
-import './index.scss'
-import Loader from 'react-loaders'
+import React from 'react';
+import './index.scss';
+import Loader from 'react-loaders';
 
 const Projects = () => {
 
-    return (<>
+    return (
+        <>
+            <span>'projects'</span>
 
-    <span>'projects'</span>
-    
-    <Loader type="ball-grid-pulse" />
-    </>);
+            <Loader type="ball-grid-pulse" />
 
+            <div className="slider">
+                <a href="#slide-1">1</a>
+                <a href="#slide-2">2</a>
+                <a href="#slide-3">3</a>
+                <a href="#slide-4">4</a>
+                <a href="#slide-5">5</a>
+
+                <div className="slides">
+                    <div id="slide-1">
+                        1
+                    </div>
+                    <div id="slide-2">
+                        2
+                    </div>
+                    <div id="slide-3">
+                        3
+                    </div>
+                    <div id="slide-4">
+                        4
+                    </div>
+                    <div id="slide-5">
+                        5
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Projects
+export default Projects;
