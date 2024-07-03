654fe871e1c05100b5a25f1a99029673d3ed7659
Nika Gedenidze <97714956+Ngedenidze@users.noreply.github.com>
2024-07-03 12:50:52 -0400

    burger menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,62 +1,102 @@
-import './index.scss';
-import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/logo-2.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser, faRocket} from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import 'normalize.css'
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../assets/logo-2.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser, faRocket, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { useSwipeable } from 'react-swipeable';
+import 'normalize.css';
+import './index.scss'; // make sure you import your SCSS
 
-// Side Bar component
 const Sidebar = () => {
-    return <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={LogoS} alt="logo" />
-        </Link>
-        {/* Navigation Bar */}
-        <nav>
-        
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} />
-                <span>Home</span>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} />
-                <span>About</span>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-            <FontAwesomeIcon icon={faRocket} />
-                <span>Projects</span>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope}/>
-                <span>Contact</span>
-            </NavLink>
-            
-        </nav>
-        <div className="social-links">
-            <ul>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.linkedin.com/in/ngedenidze/"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} color="white" />
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.github.com/Ngedenidze/"
-                >
-                  <FontAwesomeIcon icon={faGithub} color="white" />
-                </a>
-              </li>
-            </ul>
-        </div>
-        
-    </div>
-}
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-export default Sidebar   
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setDrawerOpen(false),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  });
+
+  return (
+    <div className="nav-bar" {...handlers}>
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} />
+          <span>Home</span>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} />
+          <span>About</span>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+          <FontAwesomeIcon icon={faRocket} />
+          <span>Projects</span>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span>Contact</span>
+        </NavLink>
+      </nav>
+      <div className="social-links">
+        <ul>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ngedenidze/">
+              <FontAwesomeIcon icon={faLinkedin} color="white" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://www.github.com/Ngedenidze/">
+              <FontAwesomeIcon icon={faGithub} color="white" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="burger" onClick={toggleDrawer}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+      <div className={`drawer ${drawerOpen ? 'open' : ''}`}>
+       
+        <button className="close-button" onClick={toggleDrawer}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <NavLink exact="true" activeclassname="active" to="/" onClick={toggleDrawer}>
+          Home
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/about" onClick={toggleDrawer}>
+          About
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/projects" onClick={toggleDrawer}>
+          Projects
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/contact" onClick={toggleDrawer}>
+          Contact
+        </NavLink>
+        <div className="social-links">
+          <ul>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ngedenidze/">
+                <FontAwesomeIcon icon={faLinkedin} color="white" />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://www.github.com/Ngedenidze/">
+                <FontAwesomeIcon icon={faGithub} color="white" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
